24eeb5fcfa165c92ac83ecbd5910eda7cdfc2e71
rtk707 <ritik652000@gmail.com>
2023-05-17 17:19:14 +0530

    Basic Setup

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+@app.route('/', methods=['GET'])
+def main():
+    return "Hello World"
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
